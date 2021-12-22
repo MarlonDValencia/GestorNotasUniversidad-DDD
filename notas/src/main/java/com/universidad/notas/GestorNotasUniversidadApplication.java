b66270be26c30c12fb5bd9ca5791007138a3d30a
MarlonDValencia <marlondvalencia@gmail.com>
2021-12-22 18:03:35 -0500

    Maven project initialized

---
+++ notas/src/main/java/com/universidad/notas/GestorNotasUniversidadApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorNotasUniversidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorNotasUniversidadApplication.class, args);
+	}
+
+}
---
+++ notas/src/test/java/com/universidad/notas/GestorNotasUniversidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorNotasUniversidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
